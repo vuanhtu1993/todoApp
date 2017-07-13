382dfdb242d9692f161cde37c84effe2cfeb839a
Vu Anh Tu <vuanhtu1993mta@gmail.com>
2017-07-13 15:52:03 -0700

    Initial commit

---
+++ resources/views/todoApp/index.blade.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
+</head>
+
+<body>
+<div class="background">
+<div class="container">
+    <div class="col-xs-12 col-sm-10">
+        <div class="work">
+            <h1>WORKING LIST</h1>
+        </div>
+        <div class="row">
+            <div class="workinglist">
+                <ul>
+                    <li>
+                        <div class="col-xs-9 col-sm-9 " id="worklist">
+                            ngay thu nhat lam gi
+                        </div>
+                        <div class="col-xs-3 col-sm-3" id="delete">
+                            <button type="button" class="btn">Delete</button>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-xs-6 col-sm-2">
+        <button type="button" class="btn" id="new">New</button>
+    </div>
+
+</div>
+</div>
+</body>
+</html>
